afe6d67d9d6bf4d551c9ffa07ab673f83a4e735e
Alberto Moretti <alberto.moretti@mia-platform.eu>
2023-06-26 17:53:35 +0200

    perf(core): put files in batch

--- src/promises.ts
+++ src/promises.ts
@@ -1,17 +1,18 @@
 type AsyncFunction<T> = () => Promise<T>
 
-const createQueue = <T>(fns: AsyncFunction<T>[]) => {
+const createQueue = <T>(fns: AsyncFunction<T>[], batchSize = 25) => {
   let promise: Promise<T> | undefined
   const executions: Promise<T>[] = []
 
-  for (const fn of fns) {
-    const pr = (promise ?? Promise.resolve()).then(() => fn())
+  for (let i = 0; i < fns.length; i += batchSize) {
+    const pr = (promise ?? Promise.resolve())
+      .then(async () => Promise.all(fns.slice(i, i + batchSize).map(fn => fn())) as Promise<Awaited<T>>)
     executions.push(pr)
     promise = pr
   }
 
   return {
-    flush: () => Promise.all(executions),
+    flush: () => Promise.all(executions).then((res) => res.flat()),
   }
 }
 
